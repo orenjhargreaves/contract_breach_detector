761f19e19ecece1e7abaa8b40b72c50aef598e7d
Oren Hargreaves <orenjhargreaves@gmail.com>
2024-11-18 21:29:50 +0000

    modified given contract names to align with normal notation

--- contract_breach_detector/app.py
+++ contract_breach_detector/app.py
@@ -9,23 +9,18 @@ import structured_outputs
 # Load environment variables from .env file
 load_dotenv()
 
-alum_file_path = f"/home/oren/Documents/Python/magentic_poc/contract_breach_detector/contracts/Aluminium_Contract.docx"
-print(os.path.exists(alum_file_path))
+
+test_contracts = ["Copper_contract", "Steel_contract", "Aluminium_contract"]
 doc_processor = ContractProcessor()
-
-alum_document = doc_processor.load_document(alum_file_path)
-
 terms_to_extract = structured_outputs.contract_enforcement
-
-
-alum_json = doc_processor.extract_terms(alum_document, terms_to_extract)
-
-# for i in alum_json.items():
-#     print(i)
-
 ERP_db = DataBase('db/deliveries.json', 'db/items.json')
-# print(contract_db.original_example_query())
 
-breach_detector = DetectBreach(alum_json, ERP_db)
-alum_ERP = breach_detector.searchdb()
-breach_detector.compare_details(alum_ERP)
+for i, c in enumerate(test_contracts):
+    file_path = f"/home/oren/Documents/Python/magentic_poc/contract_breach_detector/contracts/{c}.docx"
+    print(os.path.exists(file_path))
+    doc = doc_processor.load_document(file_path)
+    doc_structure = doc_processor.extract_terms(doc, terms_to_extract)
+    breach_detector = DetectBreach(doc_structure, ERP_db)
+    filtered_ERP = breach_detector.searchdb()
+    breach_detector.compare_details(filtered_ERP)
+
