3b89b3263332d4c547e2656b70818e10f5ad9ded
Oren Hargreaves <orenjhargreaves@gmail.com>
2024-11-18 21:06:42 +0000

    put commonly used LLM queries into sepate python file

--- contract_breach_detector/app.py
+++ contract_breach_detector/app.py
@@ -4,6 +4,7 @@ import os
 from contract_processor import ContractProcessor
 from DB_code import DataBase
 from breach_detector import DetectBreach
+import structured_outputs
 
 # Load environment variables from .env file
 load_dotenv()
@@ -14,16 +15,7 @@ doc_processor = ContractProcessor()
 
 alum_document = doc_processor.load_document(alum_file_path)
 
-terms_to_extract = {"info": {
-                            "supplier_name": "",
-                            "contract_number": "",
-                            },
-                    "details": {
-                                "delivery_date": "YYYY-MM-DD",
-                                "pallet_dimensions": "",
-                                "quantity": "",
-                                },            
-                    }
+terms_to_extract = structured_outputs.contract_enforcement
 
 
 alum_json = doc_processor.extract_terms(alum_document, terms_to_extract)
@@ -37,20 +29,3 @@ ERP_db = DataBase('db/deliveries.json', 'db/items.json')
 breach_detector = DetectBreach(alum_json, ERP_db)
 alum_ERP = breach_detector.searchdb()
 breach_detector.compare_details(alum_ERP)
-
-
-
-# print()
-
-# print(alum_json["contract_details"])
-
-# print(f"Q: What is the item being delivered? \n\
-#             A: {alum_json["goods_details"]["description"]} \n\
-#             Q: When is it being delivered? \n\
-#             A: {alum_json["delivery"]["delivery_date"]} \n\
-#             Q: How much of the item is being delivered? \n\
-#             A: {alum_json["goods_details"]["quantity"]} \n\
-#             Q: Are there any late delivery clauses? \n\
-#             A: {alum_json["penalties"]["late_delivery"]} \n\
-#             Q:Are there any other clauses that incur financial penalties?  \n\
-#             A: {alum_json["goods_details"]["description"]}")
---
+++ contract_breach_detector/structured_outputs.py
@@ -0,0 +1,19 @@
+
+example_questions ={
+    "What is the item being delivered": "",
+    "When is it being delivered": "",
+    "How much of the item is being delivered": "",
+    "Are there any late delivery clauses": "",
+    "Are there any other clauses that incur financial penalties": "",
+                    }
+
+contract_enforcement = {"info": {
+                            "supplier_name": "",
+                            "contract_number": "",
+                            },
+                        "details": {
+                            "delivery_date": "YYYY-MM-DD",
+                            "pallet_dimensions": "",
+                            "quantity": "",
+                                },            
+                    }
