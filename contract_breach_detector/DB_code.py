8ef634c63dd033f8d95f50edccee130677fe66a4
Oren Hargreaves <orenjhargreaves@gmail.com>
2024-11-19 20:18:11 +0000

    docstrings for DB_code

--- contract_breach_detector/DB_code.py
+++ contract_breach_detector/DB_code.py
@@ -1,32 +1,74 @@
 import duckdb
 
 class DataBase:
-    def __init__(self, deliveries_path, items_path):
-        # Connect to an in-memory DuckDB database
-        self.con = duckdb.connect()
+    """
+    A class to interact with a DuckDB in-memory database that processes JSON data
+    related to deliveries and items.
+
+    Attributes:
+        con (duckdb.DuckDBPyConnection): A connection to the DuckDB database.
+    """
+
+    def __init__(self, deliveries_path: str, items_path: str):
+        """
+        Initializes the database by connecting to an in-memory DuckDB instance 
+        and registering JSON files as database tables.
+
+        Args:
+            deliveries_path (str): The path to the deliveries JSON file.
+            items_path (str): The path to the items JSON file.
+        """
+        self.con = duckdb.connect()  # Connect to an in-memory DuckDB database
 
         # Register the JSON files as tables
         self.con.execute(
-            "CREATE VIEW deliveries AS SELECT * FROM read_json_auto('db/deliveries.json');"
+            f"CREATE VIEW deliveries AS SELECT * FROM read_json_auto('{deliveries_path}');"
         )
-        self.con.execute("CREATE VIEW items AS SELECT * FROM read_json_auto('db/items.json');")
-    
-    def query_db(self, query):
+        self.con.execute(
+            f"CREATE VIEW items AS SELECT * FROM read_json_auto('{items_path}');"
+        )
+
+    def query_db(self, query: str):
+        """
+        Executes a SQL query on the database and fetches the result as a DataFrame.
+
+        Args:
+            query (str): The SQL query to execute.
+
+        Returns:
+            pandas.DataFrame: The result of the SQL query.
+        """
         return self.con.execute(query).fetchdf()
-    
-    def lookup_contract(self, query_contract_number):
+
+    def lookup_contract(self, query_contract_number: str):
+        """
+        Looks up contract details in the database based on the given contract number.
+
+        Args:
+            query_contract_number (str): The contract number to search for.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the details of the contract.
+        """
         query = f"""
         SELECT d.*, i.*
         FROM deliveries d
         JOIN items i
         ON d.delivery_id = i.delivery_id
-        WHERE i.contract_number = '{query_contract_number}'
-        ;
+        WHERE i.contract_number = '{query_contract_number}';
         """
         return self.query_db(query)
-    
+
     def original_example_query(self):
-        # Example Query: Get all items from deliveries that are 'In Transit'
+        """
+        Retrieves all items from deliveries where the supplier is 'AluMetals'.
+
+        This is an example query showcasing the capabilities of the database.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the details of deliveries 
+                              where the supplier is 'AluMetals'.
+        """
         query = """
         SELECT 
             d.delivery_id, 
